1116ce8a2bb54e8f4c6de9160f5b76c09ac84597
EsquireGames <mark.thompson@hotmail.com.au>
2015-11-25 16:57:21 +0800

    Project Input

---
+++ Assets/Scripts/OnType1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnType1 : EnemyBasic {
+
+	public override void ChangeEnemyNumber(){
+		Camera.main.GetComponent <EnemySpawning> ().nE1 -= 1;
+		Destroy (gameObject);
+	}
+}

212411eab2f7129e8b05f56c011af8584e011f90
EsquireGames <mark.thompson@hotmail.com.au>
2015-11-25 20:18:08 +0800

    Sycing

--- Assets/Marks Side/Scripts/GUIControl.cs
+++ Assets/Marks Side/Scripts/GUIControl.cs
@@ -13,7 +13,15 @@ public class GUIControl : MonoBehaviour {
 	}
 
 	public void UnpowerTower(){
-		GameObject.FindGameObjectWithTag ("Base").GetComponent <BaseHealth>().RemoveFromList (this.gameObject);
+		GameObject.FindGameObjectWithTag ("Base").GetComponent <BaseHealth>().RemoveFromList (selectedTower);
 		Destroy (selectedTower);
+	}
+
+	public void EMPButton(){
+		GameObject.FindGameObjectWithTag ("Base").GetComponent <BaseHealth> ().power -= 5;
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		foreach (GameObject i in enemies) {
+			i.GetComponent <EnemyBasic>().ChangeEnemyNumber();
+		}
 	}
 }
